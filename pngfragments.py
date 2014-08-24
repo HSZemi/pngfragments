0990a814bc6c6a00167c47f3d400527069a6104b
Sven Zemanek <hszemi@example.com>
2014-08-24 16:35:21 +0200

    added html snippet and page

--- pngfragments.py
+++ pngfragments.py
@@ -4,6 +4,25 @@ from PIL import Image
 from random import shuffle
 import sys, os
 
+htmlsnippet = '''<div style="height: {0}px;">
+<div style="position: relative;">
+{1}
+</div>
+</div>'''
+
+pictureline = '''<div style="position: absolute; top: 0px; left: 0px; z-index: {0};"><img src="{1}" alt="" /></div>\n'''
+
+fullhtmldocument = '''<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>pngfragments</title>
+</head>
+<body>
+{0}
+</body>
+</html>'''
+
+
 if(len(sys.argv) == 3):
 	filename = sys.argv[1]
 	number_of_images = int(sys.argv[2])
@@ -21,10 +40,12 @@ if(not os.path.isfile(filename)):
 
 img = Image.open(filename)
 
-if (img.format == 'PNG' and img.mode == 'RGBA'):
+#print(img.format)
+
+if (img.format in ('PNG', 'JPEG', 'TIFF')):
 	basename = filename[:-4]
 else:
-	print("File {0} does not seem to be a .png file in RGBA mode.".format(filename))
+	print("File {0} does not seem to be a .png, .jpg, .gif or .tif file.".format(filename))
 	sys.exit(1)
 
 width = img.size[0]
@@ -38,7 +59,9 @@ pix = img.load()
 for x in range(width):
 	for y in range(height):
 		pixel = pix[x,y]
-		if(pixel[3] != 0):
+		if(img.mode == 'RGBA' and pixel[3] == 0):
+			pass
+		else:
 			colorpixels.append((x,y))
 
 print("Found {0} non-transparent pixels.".format(len(colorpixels)))
@@ -47,7 +70,7 @@ shuffle(colorpixels)
 outputs = []
 # create x new images
 for i in range(number_of_images):
-	outputs.append(Image.new(img.mode, img.size))
+	outputs.append(Image.new('RGBA', img.size))
 	outpix = outputs[i].load()
 	start = i * len(colorpixels) // number_of_images
 	end = ((i+1) * len(colorpixels) // number_of_images)
@@ -55,6 +78,23 @@ for i in range(number_of_images):
 	for (x,y) in colorpixels[start:end]:
 		outpix[x,y] = pix[x,y]
 		
-	print("Creating {0}-{1}.png".format(basename, i))
-	outputs[i].save("{0}-{1}.png".format(basename, i))
+	print("Creating output/{0}-{1}.png".format(basename, i))
+	outputs[i].save("output/{0}-{1}.png".format(basename, i))
 
+
+# create snippet
+print("Creating output/snippet.txt")
+lines = ''
+for i in range(number_of_images):
+	lines += pictureline.format(i, "{0}-{1}.png".format(basename, i))
+
+snippet = htmlsnippet.format(height + 10, lines)
+f = open("output/snippet.txt", 'w')
+print(snippet, file=f)
+f.close()
+
+# create full html document
+print("Creating output/pngfragments.html")
+f = open("output/pngfragments.html", 'w')
+print(fullhtmldocument.format(snippet), file=f)
+f.close()
